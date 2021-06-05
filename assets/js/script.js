0728821dcd62a3bcd8de46ccc28d47ed94ed1754
Geraldhi A. P. Mahayadnya <geraldiaditya1111@gmail.com>
2021-06-05 19:20:14 +0700

    adding base html UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-const nama = prompt("Siapa nama anda ?");
-
 let panggil = "Selamat Datang, ";
 
 if (nama === null || nama === ""){
@@ -14,8 +12,23 @@ sapa.innerText = panggil;
 
 function bukaSlide() {
 	let drop = document.querySelector("#down");
-	if (drop.className === "dropdown")
-		drop.className += "-mobile";
-	else
-		drop.className = "dropdown";
+	if (drop.className === "dropdown") drop.className += "-mobile";
+	else drop.className = "dropdown";
 }
+
+const menuBtn = document.getElementById("menuBtn");
+const sideNav = document.getElementById("sideNav");
+const homeBtn = document.getElementById("homeBtn")
+sideNav.style.right = "-250px";
+menuBtn.onclick = () => {
+    if (sideNav.style.right == "-250px") {
+    	sideNav.style.right = "0";
+    }
+    else {
+    	sideNav.style.right = "-250px";
+    }
+}
+const scroll = new SmoothScroll('a[href*="#"', {
+    speed: 1000,
+    speedAsDuration: true
+});
