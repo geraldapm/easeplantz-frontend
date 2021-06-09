edad4e2a40d627232299fd143f303eaafd1f0886
Geraldhi Mahayadnya <geraldiaditya1111@gmail.com>
2021-06-09 15:31:56 +0700

    fixed labeling name

--- src/components/labels.jsx
+++ src/components/labels.jsx
@@ -1,19 +1,19 @@
 const recommendations = {
     corn: [
         {
-            label: 'Corn gray leaf spot',
+            label: 'Corn Gray Leaf Spot',
             desc: 'Gunakan fungisida daun</br>Tanam tanaman yang tahan penyakit leaf spot',
         },
         {
-            label: 'corn common rust',
+            label: 'Corn Common Rust',
             desc: 'Oleskan fungisida daun<br/>Pantau perkembangan penyakit, tahap pertumbuhan tanaman dan ramalan cuaca<br/>Perhatikan jagung untuk deteksi penyakit sejak dini',
         },
         {
-            label: 'corn healthy',
+            label: 'Corn Healthy',
             desc: 'Selamat, jagung anda sehat',
         },
         {
-            label: 'corn northern leaf blight',
+            label: 'Corn Northern Leaf Blight',
             desc: 'Memilih varietas hibrida yang tahan terhadap penyakit<br/>Pastikan tanaman memiliki nutrisi yang cukup. Jangan terlalu banyak memasok nitrogen, tetapi pastikan fosfor dan kalium berada pada tingkat yang optimal.<br/>Kendalikan gulma, terutama rerumputan yang mungkin menjadi inang alternatif jamur.<br/>Kumpulkan sisa-sisa tanaman dan musnahkan dengan cara dibakar atau dikubur.<br/>Berlatih rotasi tanaman; putar dengan tanaman non-rumput.',
         },
     ],
@@ -35,43 +35,43 @@ const recommendations = {
     
     tomato: [
         {
-            label: 'Tomato___Leaf_Mold',
+            label: 'Tomato Leaf Mold',
             desc: 'Ketika menanam, gunakan benih bebas penyakit atau benih yang sudah di sembuhkan. Hancurkan semua sisa tanaman setelah panen.<br/>Sanitasi greenhouse diantara musim bercocok tanam. Gunakan kipas dan hindari menyiram daun hingga terlalu basah (siram langsung ke tanah).<br/>Apabila ada tanda - tanda penyakit, gunakan fungisida sesuai dengan aturan manufaktur untuk tanda pertama infeksi penyakit.',
         },
         {
-            label: 'Tomato___Target_Spot',
+            label: 'Tomato Target Spot',
             desc: 'Hancurkan sisa tanaman lama di akhir musim, spora bisa menginfeksi tanaman baru dari sisa-sisa tanaman lama. Buang tanaman secara benar dan letakkan tumpukan kompos di tempat yang bersuhu tinggi untuk membunuh spora.<br/>Perhatikan sirkulasi udara, target spot di tomat tumbuh subur di kondisi lembab. Tanam tomat di tempat yang mendapatkan sinar matahari cukup. Pastikan tanaman memiliki jarak dan setiap tomat memiliki sirkulasi udara yang banyak.<br/>Siram tomat di pagi hari agar daun memiliki waktu untuk mengering. Siram air di bagian tanah agar daun tidak basah. Aplikasikan mulsa ke buah untuk menghindari kontak langsung dengan tanah.',
         },
         {
-            label: 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+            label: 'Tomato Yellow Leaf Curl Virus',
             desc: 'Setelah terinfeksi virus ini, tidak ada cara untuk menyembuhkan. Tetapi, terdapat beberapa hal yang bisa dilakukan untuk menghindari tomat terkena penyakit ini.<br/>Gunakan varietas yang toleran<br/>Tanam di awal musim untuk menghindari populasi whitefly<br/>Gunakan jaring untuk menutupi tanaman dan membuat whitefly tidak bisa mencapai tanaman.<br/>Musnahkan rumput di sekitar tanaman',
         },
         {
-            label: 'Tomato___Early_blight',
+            label: 'Tomato Early Blight',
             desc: 'Belilah benih<br/>Memberi jarak untuk tanaman, menjaga sirkulasi udara agar tanaman tetap kering<br/>Gunakan fungisida organik<br/>Lakukan rotasi tanaman'
         },
         {
-            label: 'Tomato___Tomato_mosaic_virus',
+            label: 'Tomato Mosaic Virus',
             desc: 'Mosaic virus sangat susah untuk dikontrol, sanitasi merupakan praktik utama untuk mengontrol mosaic virus, alat bercocok tanam harus disterilkan selama 5 menit dan dicuci dengan deterjen yang kuat. Hancurkan bibityang tumbuh tidak normal.    ',
         },
         {
-            label: 'Tomato___Septoria_leaf_spot',
+            label: 'Tomato Septoria Leaf Spot',
             desc: 'Buang daun yang terinfeksi<br/>Gunakan fungisida organik untuk prevensi awal penyebaran penyakit<br/>Gunakan fungisida kimia untuk mengontrol penyebaran penyakit yang parah. Fungisida kimia yang tidak terlalu beracun dan ampuh adalah chlorothalonil.',
         },
         {
-            label: 'Tomato___Spider_mites Two-spotted_spider_mite',
+            label: 'Tomato Two Spotted Spider Mite',
             desc: 'Gunakan pestisida khusus untuk mites yang bernama miticide, semprotkan pada tanaman seminggu sekali, karena mites bisa menumbuhkan resistansi terhadap pestisida, maka gunakan miticide lain setiap tiga kali penggunaan.',
         },
         {
-            label: 'Tomato___Late_blight',
+            label: 'Tomato Late Blight',
             desc: 'Gunakan varietas yang imun terhadap late blight<br/>Berikan tanaman jarak antara satu dengan yang lain<br/>Hindari menyiram dari atas, siramlah tanaman langsung di tanah<br/>Perhatikan cuaca',
         },
         {
-            label: 'Tomato___healthy',
+            label: 'Tomato Healthy',
             desc: 'Selamat, tomat anda sehat!',
         },
         {
-            label: 'Tomato___Bacterial_spot',
+            label: 'Tomato Bacterial Spot',
             desc: 'Tidak bisa disembuhkan, buang dan bakar tanaman yang terkena penyakit untuk menghindari penyebaran.',
         },
     ],
